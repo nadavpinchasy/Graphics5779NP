ac3e0e98d72d27bea88292133a6680a3b0f461c6
Pinchasee <nadavp1234@gmail.com>
2019-05-14 20:33:08 +0300

    second commit

--- tests/primitives/Point3DTest.java
+++ tests/primitives/Point3DTest.java
@@ -12,7 +12,7 @@ class Point3DTest {
         Point3D p1 = new Point3D(1,2,3);
         Point3D p2 = new Point3D(3,4,5);
 
-        double result =3.4641;
-        assertEquals(result,p1.distance(p2),0.00001);
+        double result =3.46410161;
+        assertEquals(result,p1.distance(p2),0.0000001);
     }
 }
